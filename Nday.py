285e2c8a2080c3e9c1f9b644f8d677175cea8348
jmikk <buldog.buldog26@gmail.com>
2023-09-30 16:54:15 -0500

    Update Nday.py

--- Nday.py
+++ Nday.py
@@ -22,31 +22,36 @@ if os.path.exists("nukes.txt"):
     os.remove("nukes.txt")
 if os.path.exists("shields.txt"):
     os.remove("shields.txt")
+if os.path.exists("intel.txt"):
+    os.remove("shields.txt")
 
 
 with open("specs.txt", "a+") as sp:
     with open("econ.txt", "a+") as e:
         with open("nukes.txt", "a+") as n:
             with open("shields.txt", "a+") as sh:
-                with open(filename) as csv_file:
-                    csv_reader = csv.reader(csv_file)
-                    for row in csv_reader:
-                        # row.replace(" ", "_")
-                        r = requests.get(
-                            "https://www.nationstates.net/page=nukes/nation=" + row[0],
-                            headers=headers,
-                        )
-                        # r = requests.get('https://www.nationstates.net/page=nukes/nation='+"9003")
-                        soup = BeautifulSoup(r.content, "html.parser")
-                        job_elems = soup.find("span", class_="fancylike")
-                        for job_elem in job_elems:
-                            var = input(row[0] + " is " + job_elem + "...")
-                            sp.writelines(row[0] + " is " + job_elem + "\n")
-                            # print(job_elem)
-                            if "Military" in job_elem:
-                                n.writelines(row[0] + "\n")
-                                # Strategic
+                with open("shields.txt", "a+") as int:
+                    with open(filename) as csv_file:
+                        csv_reader = csv.reader(csv_file)
+                        for row in csv_reader:
+                            # row.replace(" ", "_")
+                            r = requests.get(
+                                "https://www.nationstates.net/page=nukes/nation=" + row[0],
+                                headers=headers,
+                            )
+                            # r = requests.get('https://www.nationstates.net/page=nukes/nation='+"9003")
+                            soup = BeautifulSoup(r.content, "html.parser")
+                            job_elems = soup.find("span", class_="fancylike")
+                            for job_elem in job_elems:
+                                var = input(row[0] + " is " + job_elem + "...")
+                                sp.writelines(row[0] + " is " + job_elem + "\n")
+                                # print(job_elem)
+                                if "Military" in job_elem:
+                                    n.writelines(row[0] + "\n")
+                                    # Strategic
                             if "Strategic" in job_elem:
                                 sh.writelines(row[0] + "\n")
                             if "Eco" in job_elem:
                                 sh.writelines(row[0] + "\n")
+                            if "Int" in job_elem:
+                                int.writelines(row[0] + "\n)
