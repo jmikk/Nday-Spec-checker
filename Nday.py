ee73c76c8097501d2daa52c3defb0d6e71e4e14e
jmikk <buldog.buldog26@gmail.com>
2023-09-30 23:28:05 -0500

    Update Nday.py

--- Nday.py
+++ Nday.py
@@ -3,14 +3,13 @@ from bs4 import BeautifulSoup
 import csv
 import os
 import sys
+import time
 
-Version = "3.0"
+Version = "4.0"
 print("This is Version: " + Version)
 
 user_agent = input("Enter your desired user-agent: ")
-headers = {
-    "User-Agent": user_agent + "Using 9003's Nday Spec Checker Discord: @9003"
-}
+
 
 filename = "puppet.csv"
 
@@ -22,36 +21,50 @@ if os.path.exists("nukes.txt"):
     os.remove("nukes.txt")
 if os.path.exists("shields.txt"):
     os.remove("shields.txt")
-if os.path.exists("intel.txt"):
-    os.remove("shields.txt")
 
 
 with open("specs.txt", "a+") as sp:
     with open("econ.txt", "a+") as e:
         with open("nukes.txt", "a+") as n:
             with open("shields.txt", "a+") as sh:
-                with open("shields.txt", "a+") as int:
+                with open("int.txt", "a+") as intel:
                     with open(filename) as csv_file:
                         csv_reader = csv.reader(csv_file)
                         for row in csv_reader:
                             # row.replace(" ", "_")
+                            headers = {
+                                "User-Agent": user_agent
+                                + "Used by 9003"
+                                + str(time.time),
+                            }
                             r = requests.get(
-                                "https://www.nationstates.net/page=nukes/nation=" + row[0],
+                                "https://www.nationstates.net/page=nukes/nation="
+                                + row[0],
                                 headers=headers,
                             )
                             # r = requests.get('https://www.nationstates.net/page=nukes/nation='+"9003")
                             soup = BeautifulSoup(r.content, "html.parser")
+                            elements = soup.find_all(
+                                "span", class_="smalltext", style="color:red"
+                            )
+
+                            # Check if any of the found elements contain the text 'DESTROYED'
+                            for element in elements:
+                                if "DESTROYED" in element.text:
+                                    continue
+
                             job_elems = soup.find("span", class_="fancylike")
                             for job_elem in job_elems:
-                                var = input(row[0] + " is " + job_elem + "...")
+                                # input
+                                input(row[0] + " is " + job_elem + "...")
                                 sp.writelines(row[0] + " is " + job_elem + "\n")
                                 # print(job_elem)
                                 if "Military" in job_elem:
                                     n.writelines(row[0] + "\n")
                                     # Strategic
-                            if "Strategic" in job_elem:
-                                sh.writelines(row[0] + "\n")
-                            if "Eco" in job_elem:
-                                sh.writelines(row[0] + "\n")
-                            if "Int" in job_elem:
-                                int.writelines(row[0] + "\n)
+                                if "Strategic" in job_elem:
+                                    sh.writelines(row[0] + "\n")
+                                if "Eco" in job_elem:
+                                    e.writelines(row[0] + "\n")
+                                if "Intel" in job_elem:
+                                    n.writelines(row[0] + "\n")
